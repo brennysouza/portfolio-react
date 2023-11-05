b13454186e012411fb5f3e6ac857a166533ded54
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-04 20:04:47 -0400

    added missing files as per console

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import "./Contact.css"
+
+function contact() {
+    return (
+        <div>Contact</div>
+    );
+}
+
+export default contact;
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import "./Portfolio.css"
+
+function portfolio() {
+    return (
+        <div>Portfolio</div>
+    );
+}
+
+export default portfolio;
