6c8e9c11dc13bd079c83ae0c4bd292ef3357fb2f
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-05 01:09:50 -0500

    working on portfolio section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,28 @@
 import "./Portfolio.css"
+import projects from '../../projects/projectsInfo.js';
+import Project from '../Project/Project';
+
+
 
 function portfolio() {
     return (
-        <div>Portfolio</div>
+        <div className="portfolio">
+            <div className="hero-content">
+                <h1>Portfolio</h1>
+            </div>
+            <div className="project-container">
+                {projects.map((project) => (
+                    <Project
+                        key={project.id}
+                        name={project.name}
+                        image={project.image}
+                        description={project.description}
+                        deployLink={project.deployLink}
+                        github={project.github}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,26 @@
+
+//project prop types
+Project.propTypes = {
+    deployLink: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    github: PropTypes.string.isRequired,
+};
+
+function Project(props) {
+    return (
+        <div className="img-card">
+            <a href={props.deployed}>
+                <img src={props.image} alt={props.alt} />
+                <div className="img-text">{props.title}</div>
+            </a>
+            <a href={props.github}>
+            </a>
+        </div>
+    );
+}
+
+
+
+export default Project;
