d1bc16e9748919e6ee377efd8815fad0a42e2300
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-11-08 19:10:35 -0500

    added array that contains info for each project that will be displayed in the projects page

--- src/projects/projectsInfo.js
+++ src/projects/projectsInfo.js
@@ -1,5 +1,53 @@
+// Desc: This file contains the information for each project that will be displayed on the projects page.
 const projectsInfo = [
-    
+    {
+        id: 1,
+        name: "Project 1",
+        image: "url_to_image_1",
+        description: "Description of Project 1",
+        deployLink: "url_to_deployed_project_1",
+        github: "url_to_github_repository_1",
+    },
+    {
+        id: 2,
+        name: "Project 1",
+        image: "url_to_image_1",
+        description: "Description of Project 1",
+        deployLink: "url_to_deployed_project_1",
+        github: "url_to_github_repository_1",
+    },
+    {
+        id: 3,
+        name: "Project 1",
+        image: "url_to_image_1",
+        description: "Description of Project 1",
+        deployLink: "url_to_deployed_project_1",
+        github: "url_to_github_repository_1",
+    },
+    {
+        id: 4,
+        name: "Project 1",
+        image: "url_to_image_1",
+        description: "Description of Project 1",
+        deployLink: "url_to_deployed_project_1",
+        github: "url_to_github_repository_1",
+    },
+    {
+        id: 5,
+        name: "Project 1",
+        image: "url_to_image_1",
+        description: "Description of Project 1",
+        deployLink: "url_to_deployed_project_1",
+        github: "url_to_github_repository_1",
+    },
+    {
+        id: 6,
+        name: "Project 1",
+        image: "url_to_image_1",
+        description: "Description of Project 1",
+        deployLink: "url_to_deployed_project_1",
+        github: "url_to_github_repository_1",
+    },
 ];
 
 export default projectsInfo;
